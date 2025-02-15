0672e4a822aa486669d2571b46546cb56fda19ef
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-15 12:17:29 +0000

    Rebased Photoviewer.tsx component, imageUrls separated into Utils directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
-import { PhotoViewer } from './Components/PhotoViewer/PhotoViewer'
-import './Components/PhotoViewer/PhotoViewer.css'
-import { ImageSelector, imageUrls } from './Components/ImageSelector/ImageSelector'
-import './Components/ImageSelector/ImageSelector.css'
-import { useState } from 'react'
+import './App.css';
+import { PhotoViewer } from './Components/PhotoViewer/PhotoViewer';
+import { ImageSelector } from './Components/ImageSelector/ImageSelector';
+import { imageUrls } from './Utils/imageUrls';
+import './Components/ImageSelector/ImageSelector.css';
+import { useState } from 'react';
 
 function App() {
   const [currentImage, setCurrentImage] = useState({
-    src: imageUrls[0],
+    src: imageUrls[1],
     index: 0,
   });
   
@@ -19,15 +19,14 @@ function App() {
         <p>Click on an image below to display it in a large size.</p>
       </div>
       <div className="thumbnailContainer">
-          {
-            imageUrls.map((src: string, index: number) =>
-                <ImageSelector onClick={() => setCurrentImage({src, index})} src={src} 
-                  className={
-                    currentImage.index === index? 'activeThumbnail': ''
-                  }
-                />
-            )
-          }
+          {imageUrls.map((src: string, index: number) =>
+            <ImageSelector  
+              key={index}
+              className={currentImage.index === index? 'activeThumbnail': ''}
+              src={src}
+              onClick={() => setCurrentImage({src, index})} 
+            />
+          )}
       </div>  
     </>
   )
--- src/Components/PhotoViewer/PhotoViewer.tsx
+++ src/Components/PhotoViewer/PhotoViewer.tsx
@@ -3,6 +3,7 @@ export function PhotoViewer(props: { src: string | undefined; })
     return (                
         <div className="mainImage">   
             <img src= {props.src}/>             
+            <p>Image url: {props.src}</p>
         </div>
     );
 }
